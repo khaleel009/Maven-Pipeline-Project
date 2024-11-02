100efa96c2c68287f5a9dec9dcdf5b96fd8cea23
khaleel009 <khaleel009@gmail.com>
2024-11-02 19:32:38 +0000

    src commit

---
+++ src/main/java/com/example/app/Main.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Welcome to the Awesome Stylish Maven Java Project!");
+        System.out.println("Hello, Maven World!");
+    }
+}
+
